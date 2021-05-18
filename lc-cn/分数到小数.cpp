491f6411d524f66bf2aea811bfc1fe94d6f5742d
chen <chenke876234714@gmail.com>
2021-05-18 15:26:53 +0800

    update

---
+++ lc-cn/分数到小数.cpp
@@ -0,0 +1,3 @@
+// https://leetcode-cn.com/problems/fraction-to-recurring-decimal/
+
+// 如果余数出现重复则对应小数点后的小数也会重复
---
+++ lc-cn/字符串转数组.cpp
@@ -0,0 +1,47 @@
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/string-to-integer-atoi/solution/zi-fu-chuan-zhuan-huan-zheng-shu-atoi-by-leetcode-/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+// 通过DFA(确定的有限状态自动机)实现字符串的解析
+
+class Automaton {
+    string state = "start";
+    unordered_map<string, vector<string>> table = {
+        {"start", {"start", "signed", "in_number", "end"}},
+        {"signed", {"end", "end", "in_number", "end"}},
+        {"in_number", {"end", "end", "in_number", "end"}},
+        {"end", {"end", "end", "end", "end"}}
+    }; // 状态转换表
+
+    int get_col(char c) {
+        if (isspace(c)) return 0;
+        if (c == '+' or c == '-') return 1;
+        if (isdigit(c)) return 2;
+        return 3;
+    }
+public:
+    int sign = 1;
+    long long ans = 0;
+
+    void get(char c) {
+        state = table[state][get_col(c)];
+        if (state == "in_number") {
+            ans = ans * 10 + c - '0';
+            // 确保结果不是溢出的
+            ans = sign == 1 ? min(ans, (long long)INT_MAX) : min(ans, -(long long)INT_MIN); 
+        }
+        else if (state == "signed")
+            sign = c == '+' ? 1 : -1;
+    }
+};
+
+class Solution {
+public:
+    int myAtoi(string str) {
+        Automaton automaton;
+        for (char c : str)
+            automaton.get(c); // 这里当状态为 end 时应该不用在继续了！！
+        return automaton.sign * automaton.ans;
+    }
+};
